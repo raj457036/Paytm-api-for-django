062b6d15c8fefff0b6ab04d4a4e1336f9352583a
Raj singh <rajsingh.rs457036@gmail.com>
2018-10-03 10:52:30 +0530

    model amount size changed

--- paytm/models.py
+++ paytm/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class PaytmDataBase(models.Model):
 
     order_id = models.CharField(max_length=150, unique=True)
-    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    amount = models.DecimalField(max_digits=7, decimal_places=2) # 99999.99
     checksumhash = models.CharField(max_length=255)
     txn_id = models.CharField(max_length=100)
 
